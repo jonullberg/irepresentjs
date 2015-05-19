b6862c028cbd72ce7ee7715dab87294f06b564b5
Eeshan Kumar <e3.kumar@gmail.com>
2015-05-19 13:12:08 -0700

    made populateDB work with user and issue arrays

--- populateDB.js
+++ populateDB.js
@@ -3,96 +3,82 @@
 var mongoose = require('mongoose');
 var User = require('./models/User');
 var Issue = require('./models/Issue');
+var testUser;
 
-//Create users sync
-var testUser1;
-var testUser2;
-var testUser3;
+var usersArray = [
+	{
+		username: 'testuser1',
+		email: 'testuser1@example.com',
+		password: 'password'
+	}, {
+		username: 'testuser2',
+		email: 'testuser3@example.com',
+		password: 'password'
+	}, {
+		username: 'testuser3',
+		email: 'testuser3@example.com',
+		password: 'password'
+	}, 
+];
 
-var testIssue1 = {
-  title: "issue001",
-  content: "imply dummy text of the printing",
-  votes: {
-	  up: 3,
-	  down: 8
-  },
-  date_created: "01/01/2015"
-}
-var testIssue2 = {
-  title: "issue002",
-  content: "ut also the leap into electronic t",
-  votes: {
-	  up: 45,
-	  down: 21
-  },
-  date_created: "02/01/2015"
-}
-var testIssue3 = {
-  title: "issue003",
-  content: "Latin words, consectetur, from a Lore",
-  votes: {
-    up: 32,
-    down: 811
-  },
-  date_created: "03/01/2015"
-}
-var testIssue4 = {
-	title: 'issue004', 
-	content: 'I approve of testing. Let us do more!', 
-	votes: {
-		up: 1,
-		down: 0
-	},
-	date_created: '05/11/2015'
-};
+var issuesArray = [
+	{
+	  title: "issue001",
+	  content: "imply dummy text of the printing",
+	  votes: {
+		  up: 3,
+		  down: 8
+	  },
+	  date_created: "01/01/2015"
+	}, {
+	  title: "issue003",
+	  content: "Latin words, consectetur, from a Lore",
+	  votes: {
+	    up: 32,
+	    down: 811
+	  },
+	  date_created: "03/01/2015"
+	}, {
+		title: 'issue004', 
+		content: 'I approve of testing. Let us do more!', 
+		votes: {
+			up: 1,
+			down: 0
+		},
+		date_created: '05/11/2015'
+	}
+];
 	
-function createUsers(callback) {
-	var total = 3;
+function createUsers(usersArray, callback) {
+	var total = usersArray.length;
 	var count = 0;
-	createUser('testuser1', 'testuser1@example.com', 'password', function(user) {
-		testUser1 = user;
-		console.log('user1 created');
-		checkCount();
-	});
-	createUser('testuser2', 'testuser2@example.com', 'password', function(user) {
-		testUser2 = user;
-		console.log('user2 created');
-		checkCount();
-	});
-	createUser('testuser3', 'testuser3@example.com', 'password', function(user) {
-		testUser3 = user;
-		console.log('user3 created');
-		checkCount();
+
+	usersArray.forEach(function(userObj) {
+		saveUser(userObj, function(user) {
+			console.log(user.username + ' created');
+			checkCount(user);
+		})
 	});
 
-	function checkCount() {
+	function checkCount(user) {
 		count++;
 		if (count === total) {
-			console.log('calling createIssues');
+			testUser = user;
+			console.log('calling createIssues with user: ' + user.username);
 			callback();
 		}
 	}
 }
 
-function createIssues(callback) {
-	var total = 4;
+function createIssues(issuesArray, callback) {
+	var total = issuesArray.length;
 	var count = 0;
 
-	saveIssue(testIssue1, testUser1, function() {
-		console.log('issue1 created');
-		checkCount();
-	});
-	saveIssue(testIssue2, testUser2, function() {
-		console.log('issue2 created');
-		checkCount();
-	});
-	saveIssue(testIssue3, testUser3, function() {
-		console.log('issue3 created');
-		checkCount();
-	});
-	saveIssue(testIssue4, testUser1, function() {
-		console.log('issue4 created');
-		checkCount();
+	issuesArray.forEach(function(issueObj) {
+		saveIssue(issueObj, testUser, function() {
+			console.log(issueObj.title + ' created');
+			checkCount();
+		})
 	});
 
 	function checkCount() {
@@ -103,15 +89,15 @@ function createIssues(callback) {
 	}
 }
 
-function createUser(username, email, password, callback) {
+function saveUser(user, callback) {
 	var newUser = new User({
-		username: username,
+		username: user.username,
 		basic: {
-			email: email
+			email: user.email
 		}
 	});
 
-	newUser.generateHash(password, function(err, hash) {
+	newUser.generateHash(user.password, function(err, hash) {
 		if(err) {	return console.log('Could not create hash: ' + err); }
 		newUser.basic.password = hash;
 		newUser.save(function(err, data) {
@@ -132,8 +118,8 @@ function saveIssue(issue, user, callback) {
 
 mongoose.connect('mongodb://localhost/irepresent_dev', function() {
 	console.log('Opened connection to MongoDB');
-	createUsers(function() {
-		createIssues(function() {
+	createUsers(usersArray, function() {
+		createIssues(issuesArray, function() {
 			mongoose.connection.close(function() {
 				console.log('Closed connection to MongoDB');
 			});
